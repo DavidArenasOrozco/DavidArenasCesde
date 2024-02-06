b92d632274c9bc3ac6ef8543a26ba532d3fea20d
david arenas <darenas@cesde.net>
2024-02-06 10:02:18 -0500

    fin

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -7,3 +7,10 @@ descuento = 0.25 * subtotal
 iva = 0.19 * subtotal
 
 total = (subtotal - descuento) + iva
+
+factura = print(f"""producto{producto}
+                    cantidad{cantidad}
+                    precio{precio}
+                    descuento 25%{descuento}
+                    iva 19%{iva}
+                    total{total}""")
