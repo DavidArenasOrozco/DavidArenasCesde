2210abdad821b9dce276ab5a731dcd7b30ed69c7
david arenas <darenas@cesde.net>
2024-02-06 09:59:51 -0500

    total pago

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,7 +1,9 @@
 producto = "computador"
-cantidad = "3"
+cantidad = 3
 precio = 5000
 
 subtotal = precio * cantidad
 descuento = 0.25 * subtotal
 iva = 0.19 * subtotal
+
+total = (subtotal - descuento) + iva
